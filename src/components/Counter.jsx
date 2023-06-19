06b0a14fea7137184246188217879caaf287d524
caliskanadem <ad3m.calikan@gmail.com>
2023-06-19 02:36:45 +0100

    useCallback

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,30 +1,38 @@
-import { useState, useMemo } from "react";
-import User from "./User";
-// import { Header } from "./Header";
+import { useState, useMemo, useCallback } from "react";
+// import User from "./User";
+import Header from "./Header";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [amount, setAmount] = useState(1);
   const [name, setName] = useState("Ridvan");
   console.log("Counter component re-render");
 
-  const userData = useMemo(() => {
-    return {
-      id: 1,
-      name,
-    };
-  }, [name]);
+  //   const userData = useMemo(() => {
+  //     return {
+  //       id: 1,
+  //       name,
+  //     };
+  //   }, [name]);
+  const increment = useCallback(() => {
+    setCount((prev) => prev + amount);
+  }, [amount]);
 
   return (
     <div>
-      {/* <Header count={count < 5 ? count : 5} /> */}
-      <User userData={userData} />
+      <Header increment={increment} />
+      {/* <User userData={userData} /> */}
       <hr />
       <h2>{count}</h2>
-      <button onClick={() => setCount(count + 1)}>Arttir</button>
+      <button onClick={increment}>Arttir</button>
       <hr />
-      <button onClick={() => setName(name === "Ridvan" ? "Hatice" : "Ridvan")}>
+      <div>Amount + {amount}</div>
+      <button onClick={() => setAmount(1)}>+1</button>
+      <button onClick={() => setAmount(3)}>+3</button>
+      <button onClick={() => setAmount(5)}>+5</button>
+      {/* <button onClick={() => setName(name === "Ridvan" ? "Hatice" : "Ridvan")}>
         Set Name
-      </button>
+      </button> */}
     </div>
   );
 };
