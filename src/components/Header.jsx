71b65c436d084c8f31c497af11b023d3bd830a4c
caliskanadem <ad3m.calikan@gmail.com>
2023-06-19 02:37:22 +0100

    useCallback

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { memo } from "react";
 
-export const Header = () => {
-  return <div>Header</div>;
+const Header = ({ increment }) => {
+  console.log("Header component re-render");
+  return (
+    <div>
+      Header
+      <button onClick={increment}> Arttir </button>
+    </div>
+  );
 };
+
+export default memo(Header);
